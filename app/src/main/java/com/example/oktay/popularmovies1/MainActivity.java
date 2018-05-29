be58bdacd50750cdccfda6d9bd1ad388320e4202
Oktay <03rgun@gmail.com>
2018-05-29 22:52:57 +0300

    internet connection is working in background now.

--- app/src/main/java/com/example/oktay/popularmovies1/MainActivity.java
+++ app/src/main/java/com/example/oktay/popularmovies1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.oktay.popularmovies1;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -31,12 +32,27 @@ public class MainActivity extends AppCompatActivity {
         String theMovieDbQueryType = query;
         URL theMovieDbQueryUrl = NetworkUtils.buildUrl(theMovieDbQueryType);
         mMovieListTextView.setText(theMovieDbQueryUrl.toString());
-        String theMovieDbQueryResults = null;
-        try{
-            theMovieDbQueryResults = NetworkUtils.getResponseFromHttpUrl(theMovieDbQueryUrl);
-            mMovieListTextView.setText(theMovieDbQueryResults);
-        } catch (IOException e) {
-            e.printStackTrace();
+        new TheMovieDbQueryTask().execute(theMovieDbQueryUrl);
+    }
+
+    public class TheMovieDbQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL queryUrl = urls[0];
+            String theMovieDbQueryResults = null;
+            try{
+                theMovieDbQueryResults = NetworkUtils.getResponseFromHttpUrl(queryUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return theMovieDbQueryResults;
+        }
+
+        @Override
+        protected void onPostExecute(String theMovieDbQueryResults) {
+            if (theMovieDbQueryResults != null && !theMovieDbQueryResults.equals("")){
+                mMovieListTextView.setText(theMovieDbQueryResults);
+            }
         }
     }
 
@@ -53,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
             Context context = MainActivity.this;
             String textToShow = "Popular Clicked";
             Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            // TODO 3 remove the toast message when you are done with it.
+            query = "top_rated";
+            makeTheMovieDbQuery();
             return true;
         }
         return super.onOptionsItemSelected(item);
